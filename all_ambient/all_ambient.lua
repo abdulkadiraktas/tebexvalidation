0ea4af584bd75321e643d989eccae9d5e217abba
abdulkadiraktas <abdulkadir.aktas@hotmail.com>
2023-06-09 16:34:09 +0300

    Create all_ambient.lua

---
+++ all_ambient/all_ambient.lua
@@ -0,0 +1,151 @@
+all_ambient = {
+    saint_denis_cell_ambient = {
+        {
+            ["mainplayercoords"] = vector3(2508.58, -1309.22, 48.95),
+            ["coords"] = {
+                [1] = {
+                    cam = {
+                        coords = vector3(2503.77, -1310.62, 48.4), rot = vector3(0.56, 0, -178.25)
+                    },
+                    coords = vector3(2502.78, -1310.84, 47.9),      -- First character spawn coords DO NOT CHANGE!!!!
+                    scenario = -2123759536,
+                    camoffset = vector3(0.0, 0.7, -0.3)
+                },
+                [2] = {
+                    coords = vector3(2502.78, -1310.84, 47.9),    -- Fourth character spawn coords DO NOT CHANGE!!!!
+                    cam = {
+                        coords = vector3(2504.75, -1309.98, 48.4), rot = vector3(-2.48, 0, 123.64)
+                    },
+                    scenario = -2123759499,
+                    camoffset = vector3(0.0, 0.7, -0.3)
+                },
+                [3] = {
+                    coords = vector3(2502.78, -1310.84, 47.9),     -- Third character spawn coords DO NOT CHANGE!!!!
+                    cam = {
+                        coords = vector3(2504.69, -1308.24, 49.4), rot = vector3(-6.58, 0, 142.91)
+                    },
+                    scenario = -2123759498
+                },
+                [4] = {
+                    coords = vector3(2502.78, -1310.84, 47.9),       -- Second character spawn coords DO NOT CHANGE!!!!
+                    cam = {
+                        coords = vector3(2504.69, -1308.24, 49.4), rot = vector3(-6.58, 0, 142.91)
+                    },
+                    scenario = -2123759492
+                },
+            },
+            ["cam"] = {
+                coords = vector3(2504.69, -1308.24, 49.4),
+                rot = vector3(-6.58, 0, 142.91)
+            },
+        }
+    },
+    Saint_Denis_Bazaar = {
+        {
+            mainplayercoords = {
+                ["x"] = 2830.083251953125,
+                ["y"] = -1228.7713623046875,
+                ["z"] = 43.66026306152344
+            },
+            coords = {
+                ["[1]"] = {
+                    animasyon = "weaponanims",
+                    weaponanim = "WEAPON_RIFLE_BOLTACTION",
+                    heading = -90.0,
+                    coords = {
+                    ["x"] = 2829.69,
+                    ["y"] = -1230.75,
+                    ["z"] = 46.64
+                    }
+                },
+                ["[2]"] = {
+                    coords = {
+                    ["x"] = 2829.42,
+                    ["y"] = -1229.5,
+                    ["z"] = 46.64
+                    }
+                },
+                ["[3]"] = {
+                    animasyon = "weaponanims",
+                    weaponanim = "WEAPON_RIFLE_BOLTACTION",
+                    heading = 233.0,
+                    coords = {
+                    ["x"] = 2829.51,
+                    ["y"] = -1228.5,
+                    ["z"] = 46.64
+                    }
+                },
+                ["[4]"] = {
+                    coords = {
+                    ["x"] = 2829.7,
+                    ["y"] = -1227.65,
+                    ["z"] = 46.64
+                    }
+                }
+            },
+            cam = {
+                coords = {
+                    ["x"] = 2832.3,
+                    ["y"] = -1230.88,
+                    ["z"] = 48.22
+                },
+                rot = {
+                    ["x"] = -11.68,
+                    ["y"] = 0,
+                    ["z"] = 53.65
+                }
+            }
+        },
+    },
+    Black_water_saloon = {
+        {
+            mainplayercoords = {
+                ["x"] = -814.75,
+                ["y"] = -1325.45,
+                ["z"] = 27.9
+            },
+            coords = {
+                ["[1]"] = {
+                    coords = {
+                    ["x"] = -812.35,
+                    ["y"] = -1324.79,
+                    ["z"] = 46.87
+                    }
+                },
+                ["[2]"] = {
+                    coords = {
+                    ["x"] = -813.32,
+                    ["y"] = -1325.11,
+                    ["z"] = 46.87
+                    }
+                },
+                ["[3]"] = {
+                    coords = {
+                    ["x"] = -814.14,
+                    ["y"] = -1324.75,
+                    ["z"] = 46.87
+                    }
+                },
+                ["[4]"] = {
+                    coords = {
+                    ["x"] = -814.6,
+                    ["y"] = -1323.92,
+                    ["z"] = 46.87
+                    }
+                }
+            },
+            cam = {
+                coords = {
+                    ["x"] = -811.57,
+                    ["y"] = -1322.05,
+                    ["z"] = 48.31
+                },
+                rot = {
+                    ["x"] = -3.44,
+                    ["y"] = 0,
+                    ["z"] = 131.6
+                }
+            }
+        },
+    }
+}
